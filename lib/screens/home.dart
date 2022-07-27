8edcdddd8fa5eca3e7f25d3e552c5659057b5801
steve-mir <ekechukwuemeka25@gmail.com>
2022-07-27 22:41:29 +0100

    began project

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,5 +1,6 @@
 import 'package:arkit_plugin/arkit_plugin.dart';
 import 'package:flutter/material.dart';
+import 'package:vector_math/vector_math_64.dart';
 
 class Home extends StatelessWidget {
   const Home({Key? key}) : super(key: key);
@@ -23,8 +24,11 @@ void arView(ARKitController controller) {
       materials: [
         ARKitMaterial(
           diffuse: ARKitMaterialProperty(image: 'assets/images/grid.jpg'),
+          doubleSided: true,
         ),
       ],
+      radius: 1,
     ),
+    position: Vectoc3(0,0,0);
   );
 }
